9f9bdcdb0a2ad851862eca0af2682e0912bd767d
wjoo20 <juniorosis20@gmail.com>
2021-08-30 21:42:31 -0500

    Primer proyecto React-Native

--- App.js
+++ App.js
@@ -1,21 +1,80 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react';
+import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      textValue: '',
+      count: 0,
+    };
+  }
+
+  changeTextInput = text => {
+    this.setState({
+      textValue: text
+    });
+  };
+
+  onPress = () => {
+    this.setState({
+      count: this.state.count + 1
+    });
+  }
+
+  render(){
+    return (
+      <View style = {styles.container}>
+        <View style = {styles.text}>
+          <Text>Hola Amigos</Text>
+          <StatusBar style = "auto" />
+        </View>
+        <Image source = {require('./assets/logo.png')}/>
+        <TextInput 
+          style = {{ height: 40, borderColor: 'gray', borderWidth: 1}}
+          onChangeText = {text => this.changeTextInput(text)}
+          value = {this.state.textValue}
+        />
+        <TouchableOpacity style = {styles.button} onPress = {this.onPress}>
+          <Text>Touch Here</Text>
+        </TouchableOpacity>
+        <View style = {[styles.countContainer]}>
+          <Text style = {[styles.countText]}>
+            {this.state.count}
+          </Text>
+        </View>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    paddingHorizontal: 10,
   },
+
+  text: {
+    alignItems: 'center',
+    padding: 10,
+  },
+
+  button: {
+    top: 10,
+    alignItems: 'center',
+    backgroundColor: '#DDDDDD',
+    padding: 10,
+  },
+
+  countContainer: {
+    alignItems: 'center',
+    padding: 10,
+  },
+
+  countText: {
+    color: '#FF00FF',
+  }
 });
+
